93041fc524d6c62806e8465a9010e072c4951521
Bohdan <salabay2003@gmail.com>
2022-04-03 15:00:13 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function compute()
 {
-    // Input values
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
-    var result = document.getElementById("result");
-    // Show results
-    result.innerHTML =
-        "If you deposit "+ principal +
-        ",\<br\>at an interest rate of " + rate +
-        "%\<br\>You will receive an amount of " + interest +
-        ",\<br\>in the year " + year + "\<br\>"
+    if (!validateAmount()) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        // Input values
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        var result = document.getElementById("result");
+        // Show results
+        result.innerHTML =
+            "If you deposit " + principal +
+            ",\<br\>at an interest rate of " + rate +
+            "%\<br\>You will receive an amount of " + interest +
+            ",\<br\>in the year " + year + "\<br\>"
+    }
 }
 
 // Update range value
@@ -25,8 +31,8 @@ function updateRate()
 // Check for positive values
 function validateAmount() {
     var principal = document.getElementById("principal").value;
-    if (!parseInt(principal) > 0) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
+    if (!(parseInt(principal) > 0)) {
+        return false;
     }
+    return true;
 }
